6c3fd8f1029058be46db36db0d397a293e890bce
Robert Zając <98836348+obieuz@users.noreply.github.com>
2025-01-12 03:15:00 +0100

    release

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ int get_decimal_from_roman(string number_in_roman)
 	values['M'] = 1000;
 	values['D'] = 500;
 	values['C'] = 100;
+	values['L'] = 50;
 	values['X'] = 10;
+	values['V'] = 5;
 	values['I'] = 1;
 
 	combinations["IV"] = 4;
@@ -22,39 +24,59 @@ int get_decimal_from_roman(string number_in_roman)
 	combinations["CD"] = 400;
 	combinations["CM"] = 900;
 
-	//dodac switcha na 1 wartosc
-
-
-	for (int i = 0; i < number_in_roman.length()-1; i++)
+	for (int i = 0; i < number_in_roman.length(); i++)
 	{
 		char letter = number_in_roman[i];
+
 		int letter_value = values[letter];
 
 		if (i == 0)
 		{
+
 			char next_letter = number_in_roman[i + 1];
+
 			int next_letter_value = values[next_letter];
+
 			if (letter_value < next_letter_value)
 			{
 				string combained = string(1, letter) + next_letter;
 				int value = combinations[combained];
+				i += 1;
 
 				number += value;
-				continue;
 			}
+			else
+			{
+				number += letter_value;
+			}
+			continue;
 		}
 
 		char previous_letter = number_in_roman[i - 1];
 		int previous_letter_value = values[previous_letter];
-		
-		if (letter_value < previous_letter_value)
+
+		if (letter_value > previous_letter_value)
 		{
-			string combained = string(1,letter) + previous_letter;
+			string combained = string(1, letter) + previous_letter;
 			int value = combinations[combained];
+
+			number += value;
+			i += 1;
+			continue;
+		}
+		char next_letter = number_in_roman[i + 1];
+		int next_letter_value = values[next_letter];
+
+		if (letter_value < next_letter_value)
+		{
+			string combained = string(1, letter) + next_letter;
+			int value = combinations[combained];
+			i += 1;
 
 			number += value;
 			continue;
 		}
+
 		number += letter_value;
 	}
 	return number;
@@ -75,78 +97,82 @@ int calculate(string type, int a, int b)
 		return a * b;
 	}
 	else {
-		return a / b;
+		return 0;
 	}
 }
 
-const char* get_roman_from_decimal(int number)
+string get_roman_from_decimal(int number)
 {
 	string number_in_roman;
 	while (number != 0)
 	{
-		cout << number;
-		if (number - 1000 < 0)
+		if (number - 1000 >= 0)
 		{
 			number_in_roman += "M";
 			number -= 1000;
 		}
-		else if (number - 900 < 0)
+		else if (number - 900 >= 0)
 		{
 			number_in_roman += "CM";
 			number -= 900;
 		}
-		else if (number - 500 < 0)
+		else if (number - 500 >= 0)
 		{
 			number_in_roman += "D";
 			number -= 500;
 		}
-		else if (number - 400 < 0)
+		else if (number - 400 >= 0)
 		{
 			number_in_roman += "CD";
 			number -= 400;
 		}
-		else if (number - 100 < 0)
+		else if (number - 100 >= 0)
 		{
 			number_in_roman += "C";
 			number -= 100;
 		}
-		else if (number - 90 < 0)
+		else if (number - 90 >= 0)
 		{
 			number_in_roman += "XC";
 			number -= 90;
 		}
-		else if (number - 50 < 0)
+		else if (number - 50 >= 0)
 		{
 			number_in_roman += "L";
 			number -= 50;
 		}
-		else if (number - 40 < 0)
+		else if (number - 40 >= 0)
 		{
 			number_in_roman += "XL";
 			number -= 40;
 		}
-		else if (number - 10 < 0)
+		else if (number - 10 >= 0)
 		{
 			number_in_roman += "X";
 			number -= 10;
 		}
-		else if (number - 5 < 0)
+		else if (number - 9 >= 0)
+		{
+			number_in_roman += "IX";
+			number -= 9;
+		}
+		else if (number - 5 >= 0)
 		{
 			number_in_roman += "V";
 			number -= 5;
 		}
-		else if (number - 4 < 0)
+		else if (number - 4 >= 0)
 		{
 			number_in_roman += "IV";
 			number -= 4;
 		}
-		else if (number - 1 < 0)
+		else if (number - 1 >= 0)
 		{
 			number_in_roman += "I";
 			number -= 1;
 		}
 	};
-	return number_in_roman.c_str();
+	return number_in_roman;
 }
 
 int main()
@@ -155,16 +181,22 @@ int main()
 	cin >> type;
 	cin >> first_digit_string;
 	cin >> second_digit_string;
-	//cout << type << first_digit << second_digit;
 	int first_number = get_decimal_from_roman(first_digit_string);
 	int second_number = get_decimal_from_roman(second_digit_string);
 
-	int result_number = calculate(type, first_number, second_number);
-	cout << result_number;
-	//const char* result = get_roman_from_decimal(result_number);
-	//cout << result;
-	
+	if (type == "/")
+	{
+		int result1 = first_number / second_number;
+		int result2 = first_number % second_number;
+		cout << get_roman_from_decimal(result1) << " " << get_roman_from_decimal(result2);
+		return 0;
+	}
 
-	
+	int result_number = calculate(type, first_number, second_number);
+	string result = get_roman_from_decimal(result_number);
+	cout << result;
+
+
+
 	return 0;
 }
